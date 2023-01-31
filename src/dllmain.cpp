7a0368f8540a5a929899903b35d7141e5835aaf9
dannylol19 <vldsdanny719@gmail.com>
2023-01-30 20:02:20 -0500

    Add console class for debugging

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,10 +1,27 @@
 #include <windows.h>
 #include "memory/offsets.h"
 
+class Console {
+public:
+  Console() {
+	AllocConsole();
+	freopen_s(&file, "CONOUT$", "w", stdout);
+  }
+
+  ~Console() {
+	fclose(file);
+	FreeConsole();
+  }
+
+private:
+  FILE* file = nullptr;
+};
+
 int __stdcall DllMain(HMODULE hmodule, DWORD reason, LPVOID reserved) {
   if (reason != 1) {
 	return 1;
   }
 
+  Console console();
   return 1;
 }
