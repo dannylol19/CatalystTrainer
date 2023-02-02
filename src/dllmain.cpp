0007e70932e67360d6b3ec921771f5addc914268
dannylol19 <vldsdanny719@gmail.com>
2023-02-02 01:09:48 -0500

    Added a temporary main

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,27 +1,29 @@
 #include <windows.h>
-#include "memory/offsets.h"
+#include <cstdio>
+#include <exception>
 
-class Console {
-public:
-  Console() {
-	AllocConsole();
-	freopen_s(&file, "CONOUT$", "w", stdout);
-  }
-
-  ~Console() {
-	fclose(file);
-	FreeConsole();
-  }
-
-private:
-  FILE* file = nullptr;
-};
+#include "utils/Console.h"
+#include "utils/error_msg.h"
+#include "memory/hooks.h"
 
 int __stdcall DllMain(HMODULE hmodule, DWORD reason, LPVOID reserved) {
   if (reason != 1) {
 	return 1;
   }
 
-  Console console();
+  auto console = Console::get_console();
+  printf("dll attached\n");
+
+  try {
+	hooks::enable_hooks();
+
+	// todo: thread
+
+	hooks::restore_hooks();
+  }
+  catch (std::exception e) {
+	error::msg_box(e.what(), error::MsgBoxType::Error);
+  }
+
   return 1;
 }
